8075db3d6255d1fbb3f727db40b4d3ef2aaa3337
Daniela <danielasouza0729@gmail.com>
2024-03-16 17:36:49 -0300

    feat: inplementado a função 16, palavra/frase palindromo

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,37 @@ class _MyHomePageState extends State<MyHomePage> {
       _result = _counter.toString();
     });
   }
+  /*Escreva um método que determina se uma palavra ou frase é um palíndromo, ou
+seja, a palavra pode ser lida de da esquerda para a direita ou ao contrário, ignorando
+pontuação, números e qualquer outro caracter que não seja uma letra. Considere
+também que não há diferenças entre letras maiúsculas e minúsculas.
+Exemplos de palíndromo: Ana, madam, Arara*/
+void desafio16(){
+
+
+  String verificaPalindromo = "existe grupo melhor harry flutter";
+  print('Texto original: $verificaPalindromo');
+
+  //remove os espaços em branco
+  String textoOriginalFormatado = verificaPalindromo.replaceAll(" ", "");
+
+  //Para poder fazer a comparaçao é necessário criar uma outra variável, inverter o conteúdo e comparar com o texto original formatado acima
+  String textoFormatadoInvertido = textoOriginalFormatado;
+
+  //o split quebra o text em partes (letra a letra), revertemos a ordem os itens e depois chamamos o join pra transformar em uma palavra novamente
+  textoFormatadoInvertido = textoFormatadoInvertido.split('').reversed.join();
+  
+  if ( textoOriginalFormatado == textoFormatadoInvertido){
+    _result = "$verificaPalindromo é um palíndromo";
+  
+  }else{
+    _result = "$verificaPalindromo não é um palíndromo";
+
+  }
+  setState(() {
+    _result;
+  });
+}
 
   @override
   Widget build(BuildContext context) {
